4ec882e903aa4f03df40cca917609fa2879c1c4e
janiello <janiello417@gmail.com>
2019-04-10 16:21:34 -0400

    Pseudocoded basic javascript functions. Added background and basic css styling.

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Create a 60 second countdown timer in the "time-remaining" span.
+
+// If timer hits 0...
+
+    // Calculate questions answered...
+
+    // Questions unanswered...
+    
+    // And correct answers.
+
+// If all questions are answered, calculate correct answers.
+
