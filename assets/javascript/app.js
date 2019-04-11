60558dc4b8ac08c78ca30894f266432adeac1283
janiello <janiello417@gmail.com>
2019-04-11 16:33:54 -0400

    Added start button and diretions and attempted to create timer that counts down when the button is clicked. Does not work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,26 @@
 // Create a 60 second countdown timer in the "time-remaining" span.
+var time = 60;
+var intervalId;
+document.getElementById("time-remaining").innerText = time;
+
+// Create a function for the start button that hides the "start-screen" element...
+document.getElementById("Start").onclick(start());
+
+// Brings up the trivia questions...
+
+// ...And starts the timer.
+function start() {
+    // This clears my original "time" variable...
+    clearInterval(intervalId);
+    // ...And redefines it every second by calling another function (countdown).
+    intervalId = setInterval(countdown(), 1000);
+}
+
+// This function actually changes the time variable.
+function countdown() {
+    time--;
+    document.getElementById("time-remaining").innerText(time);
+}
 
 // If timer hits 0...
 
