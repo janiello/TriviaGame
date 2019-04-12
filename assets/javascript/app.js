247c677680e339e913a5dfd62757211c7f46dce9
janiello <janiello417@gmail.com>
2019-04-12 12:59:50 -0400

    Added a stop function that stopped the timer at 0, hid the questions and revealed a score page.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,16 +7,13 @@ document.getElementById("time-remaining").innerText = time;
 document.getElementById("Start").onclick = start;
 
 function start() {
+
     // This clears my original "time" variable...
     clearInterval(intervalId);
+
     // ...And redefines it every second by calling another function (countdown).
     intervalId = setInterval(countdown, 1000);
-    
-    // This function actually changes the time variable in the "time-remaining" div.
-    function countdown() {
-        time--;
-        document.getElementById("time-remaining").innerText = time;
-    }
+
     // ...Hides the "start-screen" element...
     var hideStart = document.getElementById("start-screen");
     hideStart.style.display = "none";
@@ -25,14 +22,37 @@ function start() {
     var showQuestions = document.getElementById("questions");
     showQuestions.style.display = "block";
     
+    // This function actually changes the time variable in the "time-remaining" div.
+    function countdown() {
+        time--;
+        document.getElementById("time-remaining").innerText = time;
+        
+        // If timer hits 0...
+        if (time === 0) {
+            
+        // Run the stop function to stop the timer...
+        stop();
+
+        // Hide the "questions" div...
+        showQuestions.style.display = "none";
+        
+        // Show the "score" div...
+        var showScore = document.getElementById("score");
+        showScore.style.display = "block";
+        
+        // Calculate questions answered...
+        
+        // Questions unanswered...
+        
+        // And correct answers.
+        
+        }
+    }
 }
-// If timer hits 0...
 
-    // Calculate questions answered...
-
-    // Questions unanswered...
-    
-    // And correct answers.
-
+// Stop function to reset the timer...
+function stop() {
+    clearInterval(intervalId);
+}
 // If all questions are answered, calculate correct answers.
 
